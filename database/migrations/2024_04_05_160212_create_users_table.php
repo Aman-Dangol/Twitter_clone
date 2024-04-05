f0bfd014fe08024e9933009077fe27116cf969d0
Aman Dangol <amandangol423@gmail.com>
2024-04-05 22:52:37 +0545

    made a sign up page

--- database/migrations/2024_04_05_160212_create_users_table.php
+++ database/migrations/2024_04_05_160212_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('username', 20);
-            $table->string('email', 40);
+            $table->string('email', 40)->unique();
             $table->string('password');
             $table->timestamps();
         });
